3f65328a8223d27306dae2ddfa01ef1a4cc50488
raulescobar-g <raulescobar_g@tamu.edu>
2023-02-13 12:06:57 -0600

    changing seeded order

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,17 +2,17 @@ User.create!([
   {full_name: "Raul Escobar", email: "raulescobar_g@tamu.edu", phone_number: "9566070144"},
   {full_name: "Brandon Longuet", email: "brandon@tamu.edu", phone_number: "0001112222"}
 ])
-GroupMember.create!([
-  {group_id: 2, user_id: 2}
-])
 Group.create!([
   {name: "LoveWorld", description: "our team"},
   {name: "Another group", description: "this is another group"}
 ])
-EventMember.create!([
-  {event_id: 1, user_id: 1}
+GroupMember.create!([
+  {group_id: 2, user_id: 2}
 ])
 Event.create!([
   {name: "meeting", start: "2023-02-10 11:22:00", end: "2023-02-10 00:22:00"},
   {name: "audit", start: "2023-02-17 11:22:00", end: "2023-02-17 00:22:00"}
+])
+EventMember.create!([
+  {event_id: 1, user_id: 1}
 ])
